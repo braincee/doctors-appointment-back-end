76c10014dd4d84395f9511abed354975a4330748
braincee <kwesi938@gmail.com>
2022-11-28 10:55:35 +0000

    cleanup with rubocop

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,5 +1,4 @@
 class Doctor < ApplicationRecord
-
   has_many :appointments, dependent: :destroy
   validates :name, presence: true, length: { in: 3..50, message: 'Name\'s length should be in range of 3 to 50' }
   validates :speciality, presence: true,
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ApplicationRecord
-
   has_many :appointments, dependent: :destroy
   validates :username, precence: true, length: { minimum: 3, maximum: 50 }
   validates :email, uniqueness: true
