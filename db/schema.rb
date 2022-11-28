c0dbedace740bd466b1d066edb3a84dc6571b343
braincee <kwesi938@gmail.com>
2022-11-28 10:41:39 +0000

    Add reference user to doctor

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_28_092639) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_28_103754) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,10 +24,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_28_092639) do
   create_table "doctors", force: :cascade do |t|
     t.string "name"
     t.string "image"
-    t.string "specialization"
+    t.string "speciality"
     t.integer "fee"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_doctors_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -36,4 +38,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_28_092639) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "doctors", "users"
 end
