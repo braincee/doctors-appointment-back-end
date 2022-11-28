18d93ff23d2ea215b5da435bec1f954f7e20d77c
Bria222 <techconsultants.bree@gmail.com>
2022-11-28 17:41:41 +0300

    configure port number for rails server

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.create!(username:'amos', email:'amos@gmail.com', password:'amos123')
+User.create!(username:'stephen', email:'stephen@gmail.com', password:'stephen123')
+User.create!(username:'brenda', email:'brenda@gmail.com', password:'brenda123')
+User.create!(username:'brian', email:'brian@gmail.com', password:'brian123')
+User.create!(username:'antoney', email:'antoney@gmail.com', password:'antoney123')
+User.create!(username:'stacy', email:'stacy@gmail.com', password:'stacy123')
