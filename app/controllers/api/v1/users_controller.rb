cf2b4b8d67fa1089c304e0a61d0dbd80ffd457a1
Bria222 <techconsultants.bree@gmail.com>
2022-11-28 17:45:55 +0300

    fix linter errors

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,20 +1,20 @@
 class Api::V1::UsersController < ApplicationController
   def index
     @users = User.includes(:appointments).order('created_at desc')
-    if(@users)
+    if @users
       render json: @users, status: 200
     else
-      render json: "NO users found", status: 404
+      render json: 'NO users found', status: 404
     end
   end
 
   def show
     @user = User.where(id: params[:id]).includes(:appointments)
-     if(@user)
+    if @user
       render json: @user, status: 200
-     else
-      render json: "No user found!", status: 404
-     end
+    else
+      render json: 'No user found!', status: 404
+    end
   end
 
   def create
@@ -22,7 +22,7 @@ class Api::V1::UsersController < ApplicationController
     if @user.save
       render json: @user, status: 201
     else
-      render json: @user.error.full_messages, status: 500 
+      render json: @user.error.full_messages, status: 500
     end
   end
 
@@ -42,10 +42,10 @@ class Api::V1::UsersController < ApplicationController
     else
       render json: 'unable to update user', status: unprocessable_entity
     end
-
   end
 
   private
+
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
