94ea5885b1d65143cfff3d99c4511624cf2ed82a
braincee <kwesi938@gmail.com>
2022-11-28 10:22:39 +0000

    Add eamil uniqueness validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+
   has_many :appointments, dependent: :destroy
   validates :username, precence: true, length: { minimum: 3, maximum: 50 }
+  validates :email, uniqueness: true
 end
