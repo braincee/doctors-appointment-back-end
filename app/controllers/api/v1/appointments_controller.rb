e40bed6c4d8803c65e3a4fab6628fa5f1de698ef
Bria222 <techconsultants.bree@gmail.com>
2022-11-29 17:52:10 +0300

    fix linter errors

--- app/controllers/api/v1/appointments_controller.rb
+++ app/controllers/api/v1/appointments_controller.rb
@@ -6,16 +6,16 @@ class Api::V1::AppointmentsController < ApplicationController
     else
       render json: @appointments.errors.full_messages, status: :bad_request
     end
-   end
+  end
 
   def show
     @appointment = Appointment.where(id: params[:id])
     if @appointment
       render json: @appointment, status: :ok
-     else
+    else
       render json: @appointment.errors.full_messages, status: :bad_request
     end
-end
+  end
 
   def create
     @appointment = Appointment.new(appointment_params)
@@ -28,12 +28,12 @@ end
 
   def update
     @appointment = Appointment.find(params[:id])
-      if @appointment.update(appointment_params)
+    if @appointment.update(appointment_params)
       render json: 'appointment updated successfully'
     else
       render json: @appointment.errors.full_messages, status: :unprocessable_entity
     end
-end
+  end
 
   def destroy
     @appointment = Appointment.find(params[:id])
@@ -45,8 +45,8 @@ end
   end
 
   private
-def appointment_params
-  params.require(:appointment).permit(:city, :start_date, :end_date, :doctor_id, :user_id)
-end
 
+  def appointment_params
+    params.require(:appointment).permit(:city, :start_date, :end_date, :doctor_id, :user_id)
+  end
 end
--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -2,5 +2,4 @@ class Appointment < ApplicationRecord
   belongs_to :user
   belongs_to :doctor
   validates :city, :start_date, :end_date, :user_id, :doctor_id, presence: true
-  
 end
