0dd27d13249ea4230f05391556adb7746c0a26ca
braincee <kwesi938@gmail.com>
2022-11-29 12:56:22 +0000

    cleanup with rubocop

--- app/serializers/doctor_serializer.rb
+++ app/serializers/doctor_serializer.rb
@@ -2,8 +2,8 @@ class DoctorSerializer < ActiveModel::Serializer
   include Rails.application.routes.url_helpers
 
   attributes :id, :user_id, :name, :fee, :speciality, :image
-   has_many :appointments
-   def image
+  has_many :appointments
+  def image
     rails_blob_path(object.image, only_path: true) if object.image.attached?
   end
 end
