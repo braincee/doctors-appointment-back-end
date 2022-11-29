498f11c1704efe9b2e010281541cecfd432806b0
Bria222 <techconsultants.bree@gmail.com>
2022-11-29 16:27:37 +0300

    validate  start date and end date

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -2,5 +2,7 @@ class Appointment < ApplicationRecord
   belongs_to :user
   belongs_to :doctor
   validates :city, presence: true
-  validates :appointment_time, presence: true
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  
 end
