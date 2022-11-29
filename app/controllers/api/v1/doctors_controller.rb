c2d61c53a1158e0a20c4f94741d0297267b78597
braincee <kwesi938@gmail.com>
2022-11-29 05:04:32 +0000

    Modified doctors controller

--- app/controllers/api/v1/doctors_controller.rb
+++ app/controllers/api/v1/doctors_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::DoctorsController < ApplicationController
   def index
     @doctors = Doctor.includes(:appointments).order('created_at desc')
     if @doctors
-      render json: { status: 'Success', data: @doctors, message: 'All doctors loaded succesfully'}, status: :ok
+      render json: { status: 'SUCCESS', data: @doctors, message: 'All doctors loaded succesfully'}, status: :ok
     else
       render json: @doctors.errors, status: :bad_request
     end
@@ -12,36 +12,40 @@ class Api::V1::DoctorsController < ApplicationController
   def show
     @doctor = Doctor.where(id: params[:id]).includes(:appointments)
     if @doctor
-      render json: { status: 'Success', data: @doctors, message: 'Doctor successfully shown'}, status: :ok
+      render json: { status: 'SUCCESS', data: @doctor, message: 'Doctor successfully shown'}, status: :ok
     else 
       render json: @doctor.errors, status: :bad_request
   end
+end
 
   def create
     @doctor = Doctor.new(doctor_params)
     if @doctor.save
-      render json: { status: 'Success', data: @doctor, message: 'Doctor created successfully' }, status: :created
+      render json: { status: 'SUCCESS', data: @doctor, message: "Doctor successfully created"}, status: :created
     else 
       render json: @doctor.errors, status: :unprocessable_entity
   end
+end
 
   def destroy
-    @doctor = Doctor.where(id: params[:id]).includes(:appointments)
+     @doctor = Doctor.find(params[:id])
     if @doctor.destroy!
-      render json: { message: 'Doctor deleted successfully', data: @doctor.id }, status: :ok
+      render json: { status: "DELETED" ,message: 'Doctor deleted successfully', data: @doctor }, status: :ok
     else
       render json: @doctor.errors, status: :bad_request
   end
+end
 
   def update
     @doctor = Doctor.where(id: params[:id]).includes(:appointments)
-    if @doctor.update(user_params)
-      render json: { message: 'Doctor Update successful', data: @doctor(user_params) }, status: :ok
+    if @doctor.update(doctor_params)
+      render json: { success: "UPDATED", data: @doctor, message: 'Doctor updated successfully'}, status: :ok
     else
-      render json: { message: 'unable to update user'}, status: :unprocessable_entity
+      render json: { message: 'unable to update doctor'}, status: :unprocessable_entity
   end
+end
 
   def doctor_params
-    params.require(:doctor).permit(:name, :fee, :image, :details)
+    params.require(:doctor).permit(:user_id, :name, :fee, :image, :speciality)
   end
 end
