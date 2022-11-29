95ac0e1b213a95d2e2966ad9fd84010aba294bac
Bria222 <techconsultants.bree@gmail.com>
2022-11-29 17:46:07 +0300

    namespace appontments  endponts

---
+++ app/controllers/api/v1/appointments_controller.rb
@@ -0,0 +1,52 @@
+class Api::V1::AppointmentsController < ApplicationController
+  def index
+    @appointments = Appointment.order('created_at desc')
+    if @appointments
+      render json: @appointments, status: :ok
+    else
+      render json: @appointments.errors.full_messages, status: :bad_request
+    end
+   end
+
+  def show
+    @appointment = Appointment.where(id: params[:id])
+    if @appointment
+      render json: @appointment, status: :ok
+     else
+      render json: @appointment.errors.full_messages, status: :bad_request
+    end
+end
+
+  def create
+    @appointment = Appointment.new(appointment_params)
+    if @appointment.save
+      render json: @appointment, status: :created
+    else
+      render json: @appointment.errors.full_messages, status: :bad_request
+    end
+  end
+
+  def update
+    @appointment = Appointment.find(params[:id])
+      if @appointment.update(appointment_params)
+      render json: 'appointment updated successfully'
+    else
+      render json: @appointment.errors.full_messages, status: :unprocessable_entity
+    end
+end
+
+  def destroy
+    @appointment = Appointment.find(params[:id])
+    if @appointment.destroy
+      render json: 'appontment deleted succesfully'
+    else
+      render json: @appointment.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  private
+def appointment_params
+  params.require(:appointment).permit(:city, :start_date, :end_date, :doctor_id, :user_id)
+end
+
+end
