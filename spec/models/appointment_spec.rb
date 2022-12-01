612fdb11b6fec334991486febef96472e54c2588
Bria222 <techconsultants.bree@gmail.com>
2022-12-01 10:51:51 +0300

    add  test on creattion of new appointment

--- spec/models/appointment_spec.rb
+++ spec/models/appointment_spec.rb
@@ -2,22 +2,21 @@ require 'rails_helper'
 
 RSpec.describe Appointment, type: :model do
   describe 'Appointment model' do
-   appointment1 = Appointment.new(city: 'nairobi', start_date: '02-01-2008', end_date: '04-09-2022',doctor_id: 1)
     subject do
-        Appointment.new(city: 'nairobi', start_date: '02-01-2008', end_date: '04-09-2022',doctor_id: 1)
+      Appointment.new(city: 'nairobi', start_date: '02-01-2008', end_date: '04-09-2022', doctor_id: 1)
     end
 
     before { subject.save }
     it 'check the start_date is not blank' do
-        subject.start_date = nil
-        expect(subject).to_not be_valid
-      end
-  
-      it 'check the end_date is not blank' do
-        subject.end_date = nil
-        expect(subject).to_not be_valid
-      end
-  
+      subject.start_date = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'check the end_date is not blank' do
+      subject.end_date = nil
+      expect(subject).to_not be_valid
+    end
+
     describe 'Associations' do
       it 'has many users' do
         assc = described_class.reflect_on_association(:user)
