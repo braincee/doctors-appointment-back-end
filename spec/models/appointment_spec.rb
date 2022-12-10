d63e4f8abd051612727305ce3bcfeb74dc65de61
braincee <kwesi938@gmail.com>
2022-12-10 19:07:11 +0000

    add modifications to models spec

--- spec/models/appointment_spec.rb
+++ spec/models/appointment_spec.rb
@@ -3,17 +3,12 @@ require 'rails_helper'
 RSpec.describe Appointment, type: :model do
   describe 'Appointment model' do
     subject do
-      Appointment.new(city: 'nairobi', start_date: '02-01-2008', end_date: '04-09-2022', doctor_id: 1)
+      Appointment.new(city: 'nairobi', appointment_time: '04-09-2022', doctor_id: 1)
     end
 
     before { subject.save }
-    it 'check the start_date is not blank' do
-      subject.start_date = nil
-      expect(subject).to_not be_valid
-    end
-
-    it 'check the end_date is not blank' do
-      subject.end_date = nil
+    it 'check the appointment is not blank' do
+      subject.appointment_time = nil
       expect(subject).to_not be_valid
     end
 
