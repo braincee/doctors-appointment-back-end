8e0df90d2fea0c5f6ea7573ca535574cb015fce2
braincee <kwesi938@gmail.com>
2022-12-12 22:06:38 +0000

    Add changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,23 +6,20 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-first_user = User.create!(name:'amos', email:'amos@gmail.com', password:'amos123')
-second_user = User.create!(name:'steve', email:'stephen@gmail.com', password:'stephen123')
-third_user = User.create!(name:'brenda', email:'brenda@gmail.com', password:'brenda123')
-fourth_user = User.create!(name:'brian', email:'brian@gmail.com', password:'brian123')
-fifth_user = User.create!(name:'antoney', email:'antoney@gmail.com', password:'antoney123')
+first_user = User.create!(name:'Anthony', email:'anthony@gmail.com', password:'amos123')
+second_user = User.create!(name:'Steve', email:'stephen@gmail.com', password:'stephen123')
+third_user = User.create!(name:'Efia', email:'efia@gmail.com', password:'brenda123')
+fourth_user = User.create!(name:'Kojo', email:'kojo@gmail.com', password:'brian123')
+fifth_user = User.create!(name:'Ewurama', email:'ewurama@gmail.com', password:'antoney123')
 
 puts 'User seeds done  ..'
 puts 'Doctors seeding .....'
 
-first_doctor = Doctor.create!(user_id: first_user.id, name: 'Dr Philip', speciality: 'Gynaecology', fee: 200)
-second_doctor = Doctor.create!(user_id: second_user.id, name: 'Dr Brian', speciality: 'Surgeon', fee: 150)
+first_doctor = Doctor.create!(user_id: first_user.id, name: 'Dr Ben', speciality: 'Gynaecology', fee: 200)
+second_doctor = Doctor.create!(user_id: second_user.id, name: 'Dr August', speciality: 'Surgeon', fee: 150)
 third_doctor = Doctor.create!(user_id: second_user.id, name: 'Dr Stephen', speciality: 'Psychologist', fee: 300)
-fourth_doctor = Doctor.create!(user_id: third_user.id, name: 'Dr Louisa', speciality: 'Dentist', fee: 220)
+fourth_doctor = Doctor.create!(user_id: third_user.id, name: 'Dr Mark', speciality: 'Dentist', fee: 220)
 fifth_doctor = Doctor.create!(user_id: fourth_user.id, name: 'Dr Edmund', speciality: 'Optometrist', fee: 130)
-
-first_appointment = Appointment.create!(user_id: first_user.id, doctor_id: first_doctor.id, city: "Accra", appointment_time: "2022-12-12")
-
 
 puts 'Doctors seed successful ...'
 
